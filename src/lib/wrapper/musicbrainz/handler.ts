6d2419922f8b4a3b0344d5566a10c6d901977115
Salies <serezane@protonmail.com>
2023-01-22 00:34:53 -0300

    feat: spotify scrobble artist lookup

--- src/lib/wrapper/musicbrainz/handler.ts
+++ src/lib/wrapper/musicbrainz/handler.ts
@@ -1,4 +1,5 @@
 import { MusicBrainzApi } from 'musicbrainz-api';
+import convert from 'xml-js';
 
 const mbApi = new MusicBrainzApi({
     appName: process.env.MB_APP_NAME,
@@ -8,13 +9,15 @@ const mbApi = new MusicBrainzApi({
 
 export async function getRecordingByIsrc(isrc: string): Promise<unknown> {
     try {
-        const data = await fetch(`https://musicbrainz.org/ws/2/isrc/${isrc}?inc=artists&fmt=json`, {
+        return await fetch(`https://musicbrainz.org/ws/2/isrc/${isrc}?inc=artists`, {
             method: 'GET',
             headers: {
                 'User-Agent': `${process.env.MB_APP_NAME}/${process.env.MB_APP_VERSION} (${process.env.MB_APP_CONTACT_INFO})`,
             }
+        }).then(async res => {
+            const text = await res.text();
+            return JSON.parse(convert.xml2json(text, { compact: true, spaces: 4 }));
         });
-        return data;
     } catch (error: unknown) {
         console.log(error)
         throw error;
--- src/scrobbles/service.ts
+++ src/scrobbles/service.ts
@@ -6,11 +6,24 @@ import { getRecordingByIsrc } from '../lib/wrapper/musicbrainz/handler';
 export async function createSpotifyScrobble(scrobble: SpotifyScrobble) {
   try {
     const trackDataRaw = await getTrack(scrobble.spIdTrack);
-    let artistName: string;
+    let artistMbData: {
+      name: string;
+      mbId: string;
+    } | null = null;
 
     // Do a MusicBrainz lookup to try and find the artist
     if (trackDataRaw.external_ids.isrc) {
       const recordingDataRaw: any = await getRecordingByIsrc(trackDataRaw.external_ids.isrc);
+      const recordingList = recordingDataRaw.metadata.isrc['recording-list'];
+      // If it's only one artist, it's safe to assume it's the correct one
+      if (recordingList._attributes.count == 1 && recordingList.recording['artist-credit']['name-credit'].length == undefined){
+        const artistMbId = recordingList.recording['artist-credit']['name-credit']['artist']._attributes.id;
+        const artistMbName = recordingList.recording['artist-credit']['name-credit']['artist'].name._text;
+        artistMbData = {
+          name: artistMbName,
+          mbId: artistMbId
+        }
+      }
     }
 
     const createdScrobble = await scrobblesRepository.createSpotifyScrobble(scrobble);
